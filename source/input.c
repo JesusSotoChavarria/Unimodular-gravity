796bd05dbb54a4106b66112cb167269113a28e51
Jesus Chavarria <a329651@uach.mx>
2025-02-28 19:37:22 -0600

    Update input.c

--- source/input.c
+++ source/input.c
@@ -6184,7 +6184,8 @@ int input_default_params(struct background *pba,
    if (pba->a_start == 0. || pba->delta == 0. || pba->Delta_rho_Lambda == 0. ){
      pba->delta=0.22;
      pba->a_start=0.8;
-     pba->Delta_rho_Lambda= -0.0003;}}
+     pba->Delta_rho_Lambda= -0.0003;
+     pba->model= 1;}}
 
   /* END MODIFICATION UG */
 
