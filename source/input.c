b5d952976a7425185a5bce7b5134b0b335f5206c
JesusSChavarria <jesus.soto@correo.nucleares.unam.mx>
2025-05-31 02:04:39 -0600

    Update input.c

--- source/input.c
+++ source/input.c
@@ -5843,6 +5843,38 @@ int input_default_params(struct background *pba,
   pba->sgnK = 0;
 
   /* ** ADDITIONAL SPECIES ** */
+   /* 7) ** ADDITIONAL SPECIES ** --> Add your species here */
+  /* BEGIN MODIFICATION UG */
+  class_read_double("has_UG", pba->has_UG);
+  if(pba->has_UG==_TRUE_){
+    class_read_double("a_start",pba->a_start);
+    class_read_double("Delta_rho_Lambda",pba->Delta_rho_Lambda);
+    class_read_double("delta",pba->delta);
+    class_read_double("count_terminal", pba->count_terminal);
+
+    class_read_double("model", pba->model);}
+
+  
+ 
+   
+    /*if (pba->has_UG==1){ 
+   if (pba->a_start != 0. || pba->delta != 0. || pba->Delta_rho_Lambda != 0. ){
+
+     printf("--------------------------------------------------");
+     printf("\n");
+     printf("Unimodular gravity \n");
+     printf("delta= %f \n",pba->delta);
+     printf("a_start =%f \n",pba->a_start);
+     printf("Delta_rho_Lambda %f \n",pba->Delta_rho_Lambda);
+     printf("--------------------------------------------------");
+     printf("\n");}}*/
+
+   if (pba->has_UG !=1){
+      if (pba->a_start != 0. || pba->delta != 0. || pba->Delta_rho_Lambda != 0. ){
+        pba->has_UG==_TRUE_ ;}}
+   
+  /* END MODIFICATION UG */
+
 
   /** 7.1) Decaying CDM into Dark Radiation = dcdm+dr */
   /** 7.1.a) Current fractional density of dcdm+dr */
