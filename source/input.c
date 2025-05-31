fa286b35f09ff63806f3b6751e5f272770aaf2a2
Jesus Chavarria <a329651@uach.mx>
2025-05-31 00:10:00 -0600

    Update input.c

--- source/input.c
+++ source/input.c
@@ -2706,8 +2706,17 @@ int input_read_parameters_species(struct file_content * pfc,
 
    
 
-  /* 7) ** ADDITIONAL SPECIES ** --> Add your species here */
+   /* 7) ** ADDITIONAL SPECIES ** --> Add your species here */
+  /* BEGIN MODIFICATION UG */
+  class_read_double("delta",pba->delta);
+  class_read_double("a_start",pba->a_start);
+  class_read_double("Delta_rho_Lambda",pba->Delta_rho_Lambda);
+  class_read_double("has_UG", pba->has_UG);
+  class_read_double("count_terminal", pba->count_terminal);
 
+  class_read_double("model", pba->model);
+
+  
  
    
     /*if (pba->has_UG==1){ 
@@ -2721,16 +2730,10 @@ int input_read_parameters_species(struct file_content * pfc,
      printf("Delta_rho_Lambda %f \n",pba->Delta_rho_Lambda);
      printf("--------------------------------------------------");
      printf("\n");}}*/
-class_read_double("has_UG", pba->has_UG);
-if (pba->has_UG ==1){
-     /* BEGIN MODIFICATION UG */
-  class_read_double("delta",pba->delta);
-  class_read_double("a_start",pba->a_start);
-  class_read_double("Delta_rho_Lambda",pba->Delta_rho_Lambda);
-   /*class_read_double("count_terminal", pba->count_terminal);*/
 
-  class_read_double("model", pba->model);
-}
+   if (pba->has_UG !=1){
+      if (pba->a_start != 0. || pba->delta != 0. || pba->Delta_rho_Lambda != 0. ){
+        pba->has_UG==_TRUE_ ;}}
    
   /* END MODIFICATION UG */
 
